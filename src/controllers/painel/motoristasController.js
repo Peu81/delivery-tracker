d13efc4e8a7519ff81c68a72155bfb7ea645237e
José Pedro Costa Duda <jpcd1@aluno.ifal.edu.br>
2026-06-11 13:04:30 -0300

    Create: implementação da tela para listar entregas de um motorista.

--- src/controllers/painel/motoristasController.js
+++ src/controllers/painel/motoristasController.js
@@ -1,13 +1,15 @@
 export class motoristasController {
-    constructor(service) {
-        this.service = service;
+    constructor(motoristasService, entregasService) {
+        this.motoristasService = motoristasService;
+        this.entregasService = entregasService;
         this.index = this.index.bind(this);
         this.novo = this.novo.bind(this);
+        this.listarEntregas = this.listarEntregas.bind(this);
     }
 
     async index(req, res, next) {
         try {
-            const motoristas = await this.service.listarTodos(req.query.page, req.query.status);
+            const motoristas = await this.motoristasService.listarTodos(req.query.page, req.query.status);
             const flash = {sucesso: req.query.sucesso, erro: req.query.erro};
 
             res.render('motoristas/index', {titulo: 'Motoristas', motoristas, flash});
@@ -24,7 +26,7 @@ export class motoristasController {
 
     async novo(req, res, next) {
         try {
-            await this.service.criar(req.body);
+            await this.entregasService.criar(req.body);
             res.redirect('/painel/motoristas?sucesso=MotoristaCadastrado.')
         } catch (error) {
             res.render('motoristas/novo', {
@@ -32,6 +34,20 @@ export class motoristasController {
                 motorista: req.body,
                 erro: error.message
             })
+        }
+    }
+
+    async listarEntregas(req, res, next) {
+        try {
+            const motoristaId = Number(req.params.id);
+
+            const motorista = await this.motoristasService.buscarPorId(motoristaId);
+            const entregas = await this.entregasService.listaPorMotorista(motoristaId, req.query.status);
+
+            res.render('motoristas/entregas', {titulo: `Entregas de ${motorista.nome}`, motorista, entregas});
+
+        } catch (error) {
+            next(error);            
         }
     }
 }
--- src/routes/painel/motoristas.routes.js
+++ src/routes/painel/motoristas.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { prisma } from "../../config/dbInit.js";
 import { motoristasRepository } from "../../repositories/motoristasRepository.js";
+import { entregasRepository } from "../../repositories/entregasRepository.js";
 import { motoristasService } from "../../services/motoristasService.js";
+import { entregasService } from "../../services/entregasService.js";
 import { motoristasController as painelMotoristasController } from "../../controllers/painel/motoristasController.js";
 
 
@@ -9,12 +11,14 @@ const router = new Router();
 
 const motoristaRepo = new motoristasRepository(prisma);
 const motoristaService = new motoristasService(motoristaRepo);
-const painelMotoristaCtlr = new painelMotoristasController(motoristaService);
+const entregaRepo = new entregasRepository(prisma);
+const entregaService = new entregasService(entregaRepo, motoristaRepo);
+const painelMotoristaCtlr = new painelMotoristasController(motoristaService, entregaService);
 
 
 router.get('/', (req, res, next) => painelMotoristaCtlr.index(req, res, next));
 router.get('/novo', (req, res, next) => painelMotoristaCtlr.formularioVazio(req, res, next));
 router.post('/', (req, res, next) => painelMotoristaCtlr.novo(req, res, next));
-
+router.get('/:id/entregas', (req, res, next) => painelMotoristaCtlr.listarEntregas(req, res, next));
 
 export default router;
