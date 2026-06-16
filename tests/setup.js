089afb95fb96e2040bac1e060a205cfaf5677b06
José Pedro <jpcd1@aluno.ifal.edu.br>
2026-06-16 13:45:53 -0300

    update: pequenas correções

--- tests/setup.js
+++ tests/setup.js
@@ -1,13 +1,15 @@
 import dotenv from 'dotenv';
 
-dotenv.config({ path: '.env.test' });
-
-import { prisma } from '../src/config/dbInit.js';
+dotenv.config({ path: '.env.test', override: true });
 
 beforeAll(async () => {
-
+  const dbModule = await import('../src/config/dbInit.js');
+  prisma = dbModule.prisma;
+  await prisma.$connect();
 });
 
 afterAll(async () => {
-  await prisma.$disconnect();
+  if (prisma) {
+     await prisma.$disconnect();
+  };
 });
